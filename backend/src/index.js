2a6e8e5760cc6cf37699bfb87ca4934e1f5613a7
Đức Đạt <vanducdatt2005@gmail.com>
2026-04-10 09:30:18 +0700

    update us33 fix các lỗi không đồng bộ dữ liệu

--- backend/src/index.js
+++ backend/src/index.js
@@ -156,7 +156,7 @@ app.get("/api/invitations", authMiddleware, async (req, res) => {
       where: { userId, status: "PENDING" },
       include: {
         project: {
-          select: { name: true, key: true }
+          select: { name: true }
         }
       }
     });
